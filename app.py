27fd194137aa28ec5291246775749227bfe9d69e
emsuru <miruna.suru@gmail.com>
2024-03-29 06:41:51 +0100

    fix: docker

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-#This code defines a basic FastAPI app with a root endpoint that returns "alive" 
+#This code defines a basic FastAPI app with a root endpoint that returns "alive"
 # and a /predict endpoint expecting property data and returning a mock prediction.
 
 from fastapi import FastAPI
+import os
 from pydantic import BaseModel
 from typing import Optional
-     
+
 app = FastAPI()
 
 class Property(BaseModel):
@@ -15,9 +16,22 @@ class Property(BaseModel):
     SurfaceOfGood: Optional[int] = None
     # Add other fields as necessary
 
+
+# Set port to the env variable PORT to make it easy to choose the port on the server
+# If the Port env variable is not set, use port 8000
+PORT = os.environ.get("PORT", 8000)
+app = FastAPI(port=PORT)
+
+
 @app.get("/")
-def read_root():
-    return {"message": "alive"}
+async def root():
+    """Route that return 'Alive!' if the server runs."""
+    return {"Status": "Alive!"}
+
+@app.get("/hello")
+async def say_hello(user: str = "Anonymous"):
+    """Route that will return 'hello {user}'."""
+    return {"Message": f"Hello {user}!"}
 
 @app.post("/predict")
 def predict(property: Property):
